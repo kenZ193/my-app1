a40eb49d29a2392c6139bea745a1720aeb3e9fd4
kenZ193 <dangnguyenxuanhoai123@gmail.com>
2024-08-21 21:51:51 +0700

    screen

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://media.istockphoto.com/id/165801610/vector/farm-background.jpg?s=612x612&w=0&k=20&c=ddtdh3bA6kyIyhjZjdGbGFlYXPsCIcJkqMsNGI4471c=" className="App-background" alt="background" />
+        <div className="login-container">
+          <h2>Login</h2>
+          <form>
+            <div className="input-container">
+              <label>Username:</label>
+              <input type="text" placeholder="Enter your username" />
+            </div>
+            <div className="input-container">
+              <label>Password:</label>
+              <input type="password" placeholder="Enter your password" />
+            </div>
+            <button type="submit" className="login-button">Login</button>
+          </form>
+        </div>
       </header>
     </div>
   );
